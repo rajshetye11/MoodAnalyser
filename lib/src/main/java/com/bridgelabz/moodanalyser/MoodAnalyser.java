40d09ac8709b94591fb0afe4f22b2ee09e859059
rajshetye11 <rajshetye11@gmail.com>
2021-09-02 00:58:06 +0530

    [raj] : added ability to analyse happy or sad mood

--- lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,5 +1,11 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
-
+	public String analyseMood(String message) {
+		if(message.contains(("happy"))) {
+			return "happy";
+		}else {
+			return "sad";
+		}
+	}
 }
--- lib/src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
@@ -1,5 +1,18 @@
 package com.bridgelabz.moodanalysertest;
 
-public class MoodAnalyserTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+import com.bridgelabz.moodanalyser.MoodAnalyser;
+
+public class MoodAnalyserTest {
+	@Test
+	public void testMoodAnalysis() {
+		MoodAnalyser moodAnalyser =new MoodAnalyser();
+		String mood=moodAnalyser.analyseMood("This is a happy message");
+	
+		assertEquals("happy",mood);
+		
+	}
 }
